b81e289571d3f51072d26a98b1fde6c9398b6a37
annafalpi <anna.falceto@estudiantat.upc.edu>
2023-01-02 17:26:40 +0100

    Actualitzant llistes

--- lists/ajust_list.py
+++ lists/ajust_list.py
@@ -7,7 +7,7 @@ for lin in my_file:
     i=i+1
 print(len(data))
 mod_data = []
-folder="DATABASES/VoxCeleb/VoxCeleb1/dev"
+folder="../../DATABASES/VoxCeleb/VoxCeleb1/dev"
 
 for n in data:
     split_data=n.split('.')
@@ -15,10 +15,10 @@ for n in data:
 
 #print(len(data))
 
-#f = open('vox_celeb_1_train_labels2.txt', 'w')
-#for n in mod_data:
-#    f.write(n)
-#    f.write('\n')
+f = open('vox_celeb_1_train_labels2.txt', 'w')
+for n in mod_data:
+    f.write(n)
+    f.write('\n')
 
 f = open('REDUCTED_vox_celeb_1_train_labels.txt', 'w')
 for i in range(10):
